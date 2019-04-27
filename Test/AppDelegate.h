4076843ee7dcff3f81bbfaa6d1d1d10ea47a97e0
Nikhil Balne <nikhilkumar.balne@gmail.com>
2019-04-27 16:27:47 +0530

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by iHub on 27/04/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by iHub on 27/04/19.
+//  Copyright © 2019 iHubTechnologiesPvtLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
